0f24fb4a09e302da97f5d87c79196516d47969a4
christo <furterc@gmail.com>
2017-06-09 15:46:40 +0200

    added HDLC framer

---
+++ terminal/crc.h
@@ -0,0 +1,25 @@
+#ifndef _CRC_H_
+#define _CRC_H_
+
+#include <stdint.h>
+
+class cCrc
+{
+    static const uint16_t cCrcTable[256];
+
+    static void add_byte(uint8_t byte, uint8_t  * frame_ptr, uint32_t * frame_length);
+
+public:
+    static uint8_t crc8(uint8_t * data,uint32_t len);
+    static uint8_t crc8_update(uint8_t data,uint8_t crc);
+    static uint16_t ccitt_crc16(uint8_t data,uint16_t crc_acc);
+    static uint16_t ccitt_crc16(uint8_t * data_ptr,uint32_t len);
+
+    static const uint16_t INIT_CRC;
+    static const uint16_t GOOD_CRC;
+};
+
+
+
+
+#endif //Include Guard
